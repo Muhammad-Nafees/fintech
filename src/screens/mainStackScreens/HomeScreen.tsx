b82233d853522af996370cd8ac5c32c9c62a5c65
Muhammad Nafees <nf10018@gmail.com>
2024-08-21 04:44:35 +0500

    task completed

--- src/screens/mainStackScreens/HomeScreen.tsx
+++ src/screens/mainStackScreens/HomeScreen.tsx
@@ -1,15 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { COLORS } from '../../styles/Colors';
-import { moderateScale } from '../../utils/metrics';
-import { fontFamilies } from '../../utils/fonts';
+import {View, Text} from 'react-native';
+import React from 'react';
+import {COLORS} from '../../styles/Colors';
+import {moderateScale} from '../../utils/metrics';
+import {fontFamilies} from '../../utils/fonts';
 
 const HomeScreen = () => {
   return (
-    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-    <Text style={{color:COLORS.grey500,fontSize:moderateScale(30),fontFamily:fontFamilies.PublicSans.PublicSans_semiBold}}>HomeScreen</Text>
-  </View>
-  )
-}
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text
+        style={{
+          color: COLORS.grey500,
+          fontSize: moderateScale(30),
+          fontFamily: fontFamilies.PublicSans.PublicSans_semiBold
+        }}>
+        HomeScreen
+      </Text>
+    </View>
+  );
+};
 
 export default HomeScreen;
